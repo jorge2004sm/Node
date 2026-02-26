31742d19bac2cbf9181d59c7bf270ef77de17e52
[EDU] Jorge Sánchez Marín <jsm000.edu@innovasur.com>
2026-02-26 08:15:35 +0100

    Editado app de 02-bases

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,30 +1,31 @@
+const { getAge, getUUID } = require('./plugins')
+
 /* const {emailTemplate} = require('./js-foundation/01-template')
 
-require('./js-foundation/02-destructuring') */
-
-const {getAge, getUUID} = require('./plugins')
-
-/* const { getUserById } = require('./js-foundation/05-factory')
- */
+require('./js-foundation/02-destructuring') 
 
 
-/* const getPokemonById = require('./js-foundation/06-promises')
+
+const { getUserById } = require('./js-foundation/05-factory')
+
+
+
+const getPokemonById = require('./js-foundation/06-promises')
 
 getPokemonById(2)
 .then((pokemon) => console.log({pokemon}))
-.catch((err) => console.log({err})) */
+.catch((err) => console.log({err}))
 
 
-const {buildLogger} = require ('./plugins')
+const { buildLogger } = require('./plugins')
 const logger = buildLogger('app')
 
 logger.log('Hola mundo')
 
-/* const id = 1
+const id = 1
 
 getUserById(id, (error, user) => {
     if(error) throw new Error('User not found with id', 1)
     console.log({user})
 }) */
-
 
